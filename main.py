3661bf567e542d7a6239b508a5999bc5cdd5b1ca
Teenshin12 <65916772+Teenshin12@users.noreply.github.com>
2024-05-14 19:00:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,98 @@
-hhehehehehe
+from pymongo import MongoClient
+import gridfs
+from PIL import Image
+import json
+import io
+import matplotlib.pyplot as plt
+
+def mongo_conn():
+    try:
+        conn = MongoClient(host='127.0.0.1', port=27017)
+        print('Connected to Database')
+        return conn
+    except Exception as e:
+        print('Error, cannot connect to database')
+
+def load_data_from_json(file_path):
+    db = mongo_conn()
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        db.Wycieczki.Test_insert.insert_many(data)
+
+def insert_image(file_name, file_path):
+    db = mongo_conn()
+    try:
+        with open(file_path, "rb") as file_data:
+            data = file_data.read()
+            fs = gridfs.GridFS(db.zdjecia)
+            fs.put(data, filename=file_name)
+            print("Image inserted successfully.")
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# def display_image_names():
+#     db = mongo_conn()
+#     fs = gridfs.GridFS(db.zdjecia)
+#     files = fs.list()
+#     if files:
+#         print("Currently stored image names:")
+#         for file in files:
+#             print(file) 
+#     else:
+#         print("No images found in the database.")
+
+def display_trips_names():
+    db = mongo_conn()
+    wycieczki = db.Wycieczki.zagraniczne.find()
+    for wycieczka in wycieczki:
+        print(wycieczka['name'])
+
+def display_image(image_id):
+    db = mongo_conn()
+    fs = gridfs.GridFS(db.zdjecia)
+    image_data = fs.find_one({"_id": image_id})
+    if image_data:
+        img = Image.open(io.BytesIO(image_data.read()))
+        img.show()
+    else:
+        print(f"Image with id '{image_id}' not found in the database.")
+
+def display_images_of_trip(trip_name):
+    db = mongo_conn()
+    trip = db.Wycieczki.zagraniczne.find_one({'name': trip_name})
+    image_object_ids = trip['images']
+    if not image_object_ids:
+        print("No images found in the database.")
+        return
+    fs = gridfs.GridFS(db.zdjecia)
+    display_image(image_object_ids)
+    current_index = 0
+    while True:
+        display_image(image_object_ids[current_index])
+        user_input = input("Press 'n' for next image, 'p' for previous image, or 'q' to quit: ")
+        if user_input.lower() == 'n':
+            current_index = (current_index + 1) % len(image_object_ids)
+        elif user_input.lower() == 'p':
+            current_index = (current_index - 1) % len(image_object_ids)
+        elif user_input.lower() == 'q':
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+# Example usage:
+# file_name = 'barcelona2.jpeg'
+# file_path = 'C:/Users/filip/Desktop/barcelona2.jpeg'
+# insert_image(file_name, file_path)
+file_path = 'C:/Users/filip/Desktop/wycieczki.json'
+load_data_from_json(file_path)
+
+#display_image_names()
+# display_trips_names('C:/Users/filip/Desktop/barcelona2.jpeg')
+# trip_images_to_display = input("Enter the name of trip you want to see images of: ")
+# display_images_of_trip(trip_images_to_display)
+
+
+#image_to_display = input("Enter the name of the image you want to display: ")
+# display_image('barcelona2.jpeg')
